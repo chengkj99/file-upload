081924eb6440aa680ef95eaace684f6906984580
chengkangjian <chengkangjian@megvii.com>
2020-03-24 18:56:08 +0800

    update port 9000

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const getDirInfo = require('./getDirInfo')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9000
 const staticDir = path.resolve(__dirname, '../files')
 
 app.use(express.static('src'))
